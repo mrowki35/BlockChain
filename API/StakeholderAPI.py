feebb26330579a6c627514934a3d142fb660e049
Przemyslaw Orlikowski <orprzemek@gmail.com>
2024-12-06 20:55:58 +0100

    Added basic API classes

---
+++ API/StakeholderAPI.py
@@ -0,0 +1,11 @@
+class StakeholderAPI:
+    def createStakeHolder():
+        pass
+    def getStakeholder(self,stakeholderId: str):
+        pass
+    def getAllStakeholders(self):
+        pass
+    def updateStakeholder(self):
+        pass
+    def removeStakeholder(self):
+        pass
---
+++ API/VotingAPI.py
@@ -0,0 +1,9 @@
+class VotingAPI:
+    def createResolution(self,resolutionId: str, description: str):
+        pass
+    def castVote(self, resolutionId: str, voterId: str, voteChoice: any):
+        pass
+    def getVotingResults(self, resolutionId; str):
+        pass
+    def collectVotes(self):
+        pass
